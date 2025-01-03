7f91912c81de82034894653f5edb0892a6c18879
lazy1 <674194901@qq.com>
2025-01-03 23:10:52 +0800

    deploy

---
+++ assets/about.html-57ce46b3.js
@@ -0,0 +1,27 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, b as createBaseVNode, d as createTextVNode } from "./app-3ca80bf9.js";
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _cache[0] || (_cache[0] = [
+    createBaseVNode(
+      "h1",
+      {
+        id: "who-am-i",
+        tabindex: "-1"
+      },
+      [
+        createBaseVNode("a", {
+          class: "header-anchor",
+          href: "#who-am-i",
+          "aria-hidden": "true"
+        }, "#"),
+        createTextVNode(" Who am I？")
+      ],
+      -1
+      /* HOISTED */
+    )
+  ]));
+}
+const about_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "about.html.vue"]]);
+export {
+  about_html as default
+};
